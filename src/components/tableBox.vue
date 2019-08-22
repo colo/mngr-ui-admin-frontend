7c47dc0777e56d6f4207ccd9c0397dc2942ab5d4
Luciano Bueno <glamero@gmail.com>
2019-08-21 21:49:16 -0300

    bump

---
+++ src/components/tableBox.vue
@@ -0,0 +1,33 @@
+<template>
+  <admin-lte-box>
+    <grid-view :id="id+'.'+table+'.grid'" :components="components" :grid="grid"/>
+  </admin-lte-box>
+</template>
+<script>
+export default {
+  name: 'TableBox',
+
+  props: {
+    grid: {
+      type: [Object],
+      default: function () { return undefined }
+    },
+    components: {
+      type: [Object],
+      default: function () { return undefined }
+    },
+    data: {
+      type: [Array],
+      default: function () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+
+</script>
